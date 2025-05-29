f2e5ebfbc99cdf63af8cc5837dbf479a3f0637bd
David Silva <antoniopedrosa.silva@gmail.com>
2025-05-29 15:33:23 +0100

    mudança no nome de um input na  criação dos deploy

--- resources/views/deployment/create.blade.php
+++ resources/views/deployment/create.blade.php
@@ -63,7 +63,7 @@
             </div>
 
             <div>
-                <label for="labelName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Label Name</label>
+                <label for="labelName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selector</label>
                 <input type="text" name="labelName" id="labelName" value="{{ old('labelName', '') }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white" />
                 @error('labelName') <div class="text-red-500 text-sm mt-2">{{ $message }}</div> @enderror
             </div>
